57e08d3bdad3f034e3e2516339cb2c89b94419e4
Jonathan Rupp <jrupp@rightpoint.com>
2020-01-16 19:28:40 -0600

    direct spinning

--- src/wall.tsx
+++ src/wall.tsx
@@ -5,17 +5,29 @@ import * as _ from 'lodash';
 interface WallProps {
 }
 
+const patterns = _.range(0,12).map(x => _.range(0, 12).map(i => _.range(0, 24).map(ii => (x + i + ii) % 12)));
 
 const Wall: React.FunctionComponent<WallProps> = (props) => {
-    const [data, setData] = React.useState(_.range(0, 12).map(i => _.range(0, 24).map(ii => (i + ii) % 12)));
-    const onClick = React.useCallback(() => {
-        if (Math.random() < 0.5) {
-            const offset = Math.floor(Math.random() * 12);
-            setData(_.range(0, 12).map(i => _.range(0, 24).map(ii => (offset + i + ii) % 12)));
-        } else {
-            setData(_.range(0, 12).map(i => _.range(0, 24).map(ii => Math.floor(Math.random() * 12))));
-        }
-    }, [ setData ]);
+    const [ ix, incrementIx ] = React.useReducer((value) => (value + 1) % 12, 0);
+    const data = patterns[ix];
+    React.useEffect(() => {
+        const t = setInterval(incrementIx, 50);
+
+        return () => { clearInterval(t); }
+    }, [incrementIx]);
+
+    const onClick = React.useCallback(() => {}, []);
+
+
+    // const [data, setData] = React.useState(_.range(0, 12).map(i => _.range(0, 24).map(ii => (i + ii) % 12)));
+    // const onClick = React.useCallback(() => {
+    //     if (Math.random() < 0.5) {
+    //         const offset = Math.floor(Math.random() * 12);
+    //         setData(_.range(0, 12).map(i => _.range(0, 24).map(ii => (offset + i + ii) % 12)));
+    //     } else {
+    //         setData(_.range(0, 12).map(i => _.range(0, 24).map(ii => Math.floor(Math.random() * 12))));
+    //     }
+    // }, [ setData ]);
 
     return (
         <div onClick={onClick}>
